a4cee124cea67534155dffeb7a9ff6e5d95516d2
YehonatanHayat <yonatan.hayat@gmail.com>
2024-03-26 22:43:19 +0200

    26/3/24

--- models/posts.js
+++ models/posts.js
@@ -1,25 +1,4 @@
-// const mongoose = require('mongoose');
 
-// const post = new mongoose.Schema({
-//   content: {
-//     type: String,
-//     required: true
-//   },
-//   author: {
-//     type: String,
-//     required: true
-//   },
-//   date: {
-//     type: Date,
-//     required: true
-//   },
-//   pic: {
-//     type: String
-    
-//   }
-// });
-
-// module.exports = mongoose.model('Post', post);
 const mongoose = require('mongoose');
 
 const post = new mongoose.Schema({
